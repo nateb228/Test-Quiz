e27d31fa2197c264df54ae4a4d88eb31d774e416
nateb228 <nathan@codespace.co.za>
2018-01-26 22:51:00 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <div id="app">
+    <div id="panel">
+      <!-- #failing at getting these dumb transitions to work  -->
+      <!-- <transition name="slide"> -->
+        <Hello @buttonClicked="handleClick" v-show="!questionTime"></Hello>
+      <!-- </transition> -->
+      <!-- <transition-group name="slide"> -->
+        <Question @backClicked="goBack" @answerStored="nextQuestion" v-show="questionTime" v-if="currentQuestion === question" v-for="(question, index) in questions" v-bind:key="index" :question="question" :index="index"></Question>
+      <!-- <transition-group> -->
+        <Result v-show="showResult" :picture="this.picture" :address="this.address" :description="this.description"></Result>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Hello from './components/Hello'
+import Question from './components/Question'
+import Result from './components/Result'
+
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      questionTime: false, // activates after homepage
+      questions: [],
+      currentQuestion: null,
+      questionIndex: 0,
+      showResult: false,
+      responses: [],
+      resultKeys: [],
+      picture: null,
+      address: null,
+      description: null
+    }
+  },
+
+  mounted () {
+    console.log('App -> mounted.')
+    axios.get('static/quiz.json')
+      .then((response) => {
+        this.questions = response.data
+      })
+  },
+
+  components: {
+    Hello,
+    Question,
+    Result
+  },
+
+  methods: {
+    handleClick () {  // triggers after "Take the quiz" button is clicked
+      console.log('App -> button clicked.')
+      this.questionTime = true
+      this.currentQuestion = this.questions[this.questionIndex]
+    },
+
+    goBack () {   // go back to previous question
+      this.questionIndex--
+      console.log('App -> questionIndex: ' + this.questionIndex)
+      this.responses.splice(this.questionIndex, 1)  // removes previously recorded answers
+      this.resultKeys.splice(this.questionIndex, 1)
+      this.currentQuestion = this.questions[this.questionIndex]
+    },
+
+    nextQuestion (a) {  // triggers when an answer choice is clicked
+      console.log('App -> nextQuestionClicked')
+      this.responses.push(a)
+      this.resultKeys.push(a.charAt(0)) // resultKeys stores the first letter of each answer choice
+      this.questionIndex++
+      if (this.questionIndex === 9) { // shows Result component after question 9
+        this.calculateResult()
+        this.showResult = true
+      }
+      this.currentQuestion = this.questions[this.questionIndex]
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: rgb(255, 255, 255);
+  background-image: url("/static/images/sombrero.jpg");
+  display: flex;
+  /*border: 5px solid red;*/
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+}
+
+#panel{
+  background: rgba(255, 255, 255, 0.6);
+  height: 80vh;
+  width: 70vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  /*border: 5px solid red;*/
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  position: absolute;
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+  position: absolute;
+}
+
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="result">
+    <h1>You are the <span class="blue"> {{ this.picture }} Galaxy</span>!</h1>
+    <img :src="address"></img>
+    <p class="description">{{ this.description }}<p>
+    <p id="credit">Image source: <a href="http://nasa.gov" target="_newtab">NASA</a></p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: [
+    'picture',
+    'address',
+    'description'
+  ],
+
+  data () {
+    return {
+      spaceImage: '/vue-project/static/images/' + this.picture + '.jpg'
+    }
+  }
+}
+
+</script>
+
+<style>
+
+.result {
+  /*padding-top: 20px;*/
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  /*border: 1px solid red;*/
+}
+
+h1 {
+  font-size: 5vmin;
+  text-align: center;
+}
+
+img {
+  width: 40vmin;
+  height: auto;
+}
+
+.description {
+  color: black;
+  font-size: 3vmin; /* make this em later, also test for responsiveness */
+  font-weight: bold;
+  text-align: center;
+}
+
+.blue {
+  color: rgb(15, 82, 186);
+}
+
+#credit {
+  align-self: flex-end;
+  font-size: 3vmin;
+  font-weight: normal;
+  font-style: italic;
+  color: black;
+}
+
+@media only screen and (min-device-width : 375px)
+    and (max-device-width : 667px)
+    and (width : 375px)
+    and (height : 559px)
+    and (orientation : portrait)
+    and (color : 8)
+    and (device-aspect-ratio : 375/667)
+    and (aspect-ratio : 375/559)
+    and (device-pixel-ratio : 2)
+    and (-webkit-min-device-pixel-ratio : 2) {
+
+  .result {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  h1 {
+    font-size: 5vmin;
+  }
+
+  img {
+    height: 50vmin;
+    width: auto;
+  }
+
+  .description {
+    font-size: 2vh;
+    color: black;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  #credit {
+    align-self: flex-end;
+    font-size: 2vh;
+    font-weight: normal;
+    font-style: italic;
+    color: black;
+  }
+}
+
+
+</style>
